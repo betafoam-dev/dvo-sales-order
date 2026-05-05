04e9802872a18f9614500e9068e8492344fbfc65
Jhenrich <jhenrichroquios000@gmail.com>
2026-05-05 21:25:03 +0800

    push

--- edit.php
+++ edit.php
@@ -636,8 +636,7 @@ $currentStatus = $data['status'];
                                             <div class="sd-item"
                                                 data-value="<?= $inv['id'] ?>"
                                                 data-code="<?= htmlspecialchars($inv['stock_code']) ?>"
-                                                data-name="<?= htmlspecialchars($inv['stock_name']) ?>"
-                                                data-description="<?= htmlspecialchars($inv['stock_description']) ?>"
+                                                data-name="<?= htmlspecialchars($inv['stock_description']) ?>"
                                                 data-uom="<?= htmlspecialchars($inv['uom']) ?>"
                                                 data-label="<?= htmlspecialchars($inv['stock_code'] . ' - ' . $inv['stock_description']) ?>">
                                                 <?= htmlspecialchars($inv['stock_code'] . ' - ' . $inv['stock_description']) ?>
